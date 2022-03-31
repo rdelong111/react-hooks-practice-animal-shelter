b73c05a227ce2ad8a2dd6819c08131a4198dacb3
Ryan DeLong <rdelong111@gmail.com>
2022-03-30 19:31:11 -0500

    commit tree a51e855f79a64ee39f0d425ea08a624b9e86ae9c

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
-  const [filters, setFilters] = useState({ type: "all" });
+  const [filter, setFilter] = useState('all');
+  const [currentFilt, changeFilt] = useState('all');
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pets${filter === 'all' ? '' : `?type=${filter}`}`)
+      .then((r) => r.json())
+      .then((thePets) => setPets(thePets));
+  }, [filter]);
+
+  function handleAdoptPet(ID) {
+    fetch(`http://localhost:3001/pets/${ID}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({isAdopted: true})
+    })
+      .then((r) => r.json())
+      .then((updatedPet) => {
+        const updatedList = pets.map((pet) => {
+          return pet.id === ID ? updatedPet : pet;
+        });
+        setPets(updatedList);
+      });
+  }
 
   return (
     <div className="ui container">
@@ -15,10 +38,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters
+              onChangeType={(e) => changeFilt(e.target.value)}
+              onFindPetsClick={() => setFilter(currentFilt)}
+            />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={handleAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,12 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({pets, onAdoptPet}) {
+  const petList = pets.map((pet) => (
+    <Pet key={pet.id} pet={pet} onAdoptPet={onAdoptPet} />
+  ));
+
+  return <div className="ui cards">{petList}</div>;
 }
 
 export default PetBrowser;
